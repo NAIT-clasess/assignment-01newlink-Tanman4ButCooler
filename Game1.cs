233b65cf86a9037712de721beb4cc8f0ceb2fb65
Tanman4ButCooler <107323433+Tanman4ButCooler@users.noreply.github.com>
2026-01-31 21:26:16 -0700

    Static Image with Automatic Movement

--- Game1.cs
+++ Game1.cs
@@ -10,11 +10,15 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    // Added for background and text
     private Texture2D _background;
     private SpriteFont _font;
     private int _windowWidth;
     private int _windowHeight;
+    
+
+    private Texture2D _staticImage;
+    private Vector2 _autoMovePosition;
+    private float _autoSpeed = 80f;
 
     public Game1()
     {
@@ -30,6 +34,8 @@ public class Game1 : Game
     {
         _windowWidth = GraphicsDevice.Viewport.Width;
         _windowHeight = GraphicsDevice.Viewport.Height;
+        
+        _autoMovePosition = new Vector2(0, 100);
 
         base.Initialize();
     }
@@ -37,14 +43,23 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
         _background = Content.Load<Texture2D>("Background");
         _font = Content.Load<SpriteFont>("Sansfont");
+        
+        _staticImage = Content.Load<Texture2D>("Idle");
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _autoMovePosition.X += _autoSpeed * deltaTime;
+        
+        if (_autoMovePosition.X > _windowWidth)
+            _autoMovePosition.X = -_staticImage.Width;
 
         base.Update(gameTime);
     }
@@ -55,12 +70,14 @@ public class Game1 : Game
 
         _spriteBatch.Begin();
 
-        // Draw background
         _spriteBatch.Draw(
             _background,
             new Rectangle(0, 0, _windowWidth, _windowHeight),
             Color.White
         );
+
+        // Draws static image with automatic movement
+        _spriteBatch.Draw(_staticImage, _autoMovePosition, Color.White);
 
         // Draw text
         _spriteBatch.DrawString(
